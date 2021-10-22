7fd49d8376b38fa10e6daf2b7985253728ceecd8
Droneship <81514881+Seifood@users.noreply.github.com>
2021-10-22 12:47:51 +0200

    Update and rename cipher_gauguin.mk to fluid_gauguin.mk
    trying fluid

--- fluid_gauguin.mk
+++ fluid_gauguin.mk
@@ -8,10 +8,10 @@
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cipher_gauguin
+PRODUCT_NAME := fluid_gauguin
 PRODUCT_DEVICE := gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
