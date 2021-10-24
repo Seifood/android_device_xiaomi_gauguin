73997870816da0760a3d5441e8f8b5113569f338
Droneship <81514881+Seifood@users.noreply.github.com>
2021-10-24 17:06:33 +0200

    aex

--- aosp_gauguin.mk
+++ aosp_gauguin.mk
@@ -8,10 +8,10 @@
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_gauguin
+PRODUCT_NAME := aosp_gauguin
 PRODUCT_DEVICE := gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
